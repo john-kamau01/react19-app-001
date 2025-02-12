ca28a53fa15eeec15103ac1899c183c553805ab9
John Kamau <johnwaweru092@gmail.com>
2025-02-12 11:02:27 +0000

    first react app

---
+++ first-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Profile from './Profile';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to our first React App</h1>
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
